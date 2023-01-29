7624187088048e32da615a7d40d7fcc040a90539
Alan Silva <123318858+howstrangeoficial@users.noreply.github.com>
2023-01-29 20:14:37 -0300

    Configurando o babell para compilar o typescript

---
+++ babel.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current'
+        }
+      }
+    ],
+    '@babel/preset-typescript'
+  ],
+  plugins: [
+    ['module-resolver', {
+      alias: {
+        '@app': './src',
+      }
+    }]
+  ],
+  ignore: [
+    '**/*.spec.ts'
+  ]
+}
